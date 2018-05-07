840ead47318b7a7e9be5042324d3c0a27259ad08
Daniel Y. Guerrero <yanez.daniel0101@gmail.com>
2018-05-07 01:14:55 -0500

    ac

--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="home"),
-    url(r'/about', views.about, name="about"),
-    url(r'/projects', views.projects, name="projects"),
-    url(r'/resume', views.resume, name="resume")
+    url(r'about', views.about, name="about"),
+    url(r'projects', views.projects, name="projects"),
+    url(r'resume', views.resume, name="resume")
 ]
